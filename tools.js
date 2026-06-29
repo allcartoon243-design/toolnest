767ac573b0710b4dfdff103b6bea4bc81e8f077c
allcartoon243-design <allcartoon243@gmail.com>
2026-06-30 02:52:29 +0500

    Delete tools.js

--- tools.js
+++
@@ -1,5 +0,0 @@
-// tools.js
-export async function removeBackgroundWithImgly(file) {
-    const { removeBackground } = await import("https://cdn.jsdelivr.net/npm/@imgly/background-removal@latest/dist/index.js");
-    return await removeBackground(file, { publicPath: "/", model: "small" });
-}
